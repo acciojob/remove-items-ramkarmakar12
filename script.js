d578500c78d3f539d778dbb50d1afbb443742ef6
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-03-21 08:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const select = document.getElementById("colorSelect");
+    const button = document.querySelector("input[type='button']");
+
+    button.addEventListener("click", function () {
+        if (select.selectedIndex !== -1) {
+            select.remove(select.selectedIndex);
+        }
+    });
+});
